3a115319ac02e7096a0ac0fa97b7010ca567ed0a
Manali0806 <manalikundu824@gmail.com>
2023-11-16 12:11:13 +0530

    Credit Card Management System

---
+++ DATABASE/creditcardmanagementsystem_db.sql
@@ -0,0 +1,324 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2023 at 07:37 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `creditcardmanagementsystem_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_acc_types`
+--
+
+CREATE TABLE `ib_acc_types` (
+  `acctype_id` int(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` longtext NOT NULL,
+  `rate` varchar(200) NOT NULL,
+  `code` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ib_acc_types`
+--
+
+INSERT INTO `ib_acc_types` (`acctype_id`, `name`, `description`, `rate`, `code`) VALUES
+(1, 'Savings', '<p>Savings accounts&nbsp;are typically the first official bank account anybody opens. Children may open an account with a parent to begin a pattern of saving. Teenagers open accounts to stash cash earned&nbsp;from a first job&nbsp;or household chores.</p><p>Savings accounts are an excellent place to park&nbsp;emergency cash. Opening a savings account also marks the beginning of your relationship with a financial institution. For example, when joining a credit union, your &ldquo;share&rdquo; or savings account establishes your membership.</p>', '20', 'ACC-CAT-4EZFO'),
+(2, ' Retirement', '<p>Retirement accounts&nbsp;offer&nbsp;tax advantages. In very general terms, you get to&nbsp;avoid paying income tax on interest&nbsp;you earn from a savings account or CD each year. But you may have to pay taxes on those earnings at a later date. Still, keeping your money sheltered from taxes may help you over the long term. Most banks offer IRAs (both&nbsp;Traditional IRAs&nbsp;and&nbsp;Roth IRAs), and they may also provide&nbsp;retirement accounts for small businesses</p>', '10', 'ACC-CAT-1QYDV'),
+(4, 'Recurring deposit', '<p><strong>Recurring deposit account or RD account</strong> is opened by those who want to save certain amount of money regularly for a certain period of time and earn a higher interest rate.&nbsp;In RD&nbsp;account a&nbsp;fixed amount is deposited&nbsp;every month for a specified period and the total amount is repaid with interest at the end of the particular fixed period.&nbsp;</p><p>The period of deposit is minimum six months and maximum ten years.&nbsp;The interest rates vary&nbsp;for different plans based on the amount one saves and the period of time and also on banks. No withdrawals are allowed from the RD account. However, the bank may allow to close the account before the maturity period.</p><p>These accounts can be opened in single or joint names. Banks are also providing the Nomination facility to the RD account holders.&nbsp;</p>', '15', 'ACC-CAT-VBQLE'),
+(5, 'Fixed Deposit Account', '<p>In <strong>Fixed Deposit Account</strong> (also known as <strong>FD Account</strong>), a particular sum of money is deposited in a bank for specific&nbsp;period of time. It&rsquo;s one time deposit and one time take away (withdraw) account.&nbsp;The money deposited in this account can not be withdrawn before the expiry of period.&nbsp;</p><p>However, in case of need,&nbsp; the depositor can ask for closing the fixed deposit prematurely by paying a penalty. The penalty amount varies with banks.</p><p>A high interest rate is paid on fixed deposits. The rate of interest paid for fixed deposit vary according to amount, period and also from bank to bank.</p>', '40', 'ACC-CAT-A86GO'),
+(7, 'Current account', '<p><strong>Current account</strong> is mainly for business persons, firms, companies, public enterprises etc and are never used for the purpose of investment or savings.These deposits are the most liquid deposits and there are no limits for number of transactions or the amount of transactions in a day. While, there is no interest paid on amount held in the account, banks charges certain &nbsp;service charges, on such accounts. The current accounts do not have any fixed maturity as these are on continuous basis accounts.</p>', '20', 'ACC-CAT-4O8QW'),
+(8, 'Credit Card Accounts', '<p>Credit Card Accounts</p>', '10.75', 'ACC-CAT-95NB7'),
+(9, 'Credit Card Accounts', '<p>Credit Card Accounts</p>', '10.75', 'ACC-CAT-DEBXL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_admin`
+--
+
+CREATE TABLE `ib_admin` (
+  `admin_id` int(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `number` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `profile_pic` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ib_admin`
+--
+
+INSERT INTO `ib_admin` (`admin_id`, `name`, `email`, `number`, `password`, `profile_pic`) VALUES
+(2, 'System Administrator', 'admin@mail.com', 'iBank-ADM-0516', '036d0ef7567a20b5a4ad24a354ea4a945ddab676', 'admin-icn.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_bankaccounts`
+--
+
+CREATE TABLE `ib_bankaccounts` (
+  `account_id` int(20) NOT NULL,
+  `acc_name` varchar(200) NOT NULL,
+  `account_number` varchar(200) NOT NULL,
+  `acc_type` varchar(200) NOT NULL,
+  `acc_rates` varchar(200) NOT NULL,
+  `acc_status` varchar(200) NOT NULL,
+  `acc_amount` varchar(200) NOT NULL,
+  `client_id` varchar(200) NOT NULL,
+  `client_name` varchar(200) NOT NULL,
+  `client_national_id` varchar(200) NOT NULL,
+  `client_phone` varchar(200) NOT NULL,
+  `client_number` varchar(200) NOT NULL,
+  `client_email` varchar(200) NOT NULL,
+  `client_adr` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ib_bankaccounts`
+--
+
+INSERT INTO `ib_bankaccounts` (`account_id`, `acc_name`, `account_number`, `acc_type`, `acc_rates`, `acc_status`, `acc_amount`, `client_id`, `client_name`, `client_national_id`, `client_phone`, `client_number`, `client_email`, `client_adr`, `created_at`) VALUES
+(20, 'Manali Kundu', '561374890', 'Select Any iBank Account types', '', 'Active', '0', '11', 'Manali Kundu', 'staff@mail.com', '08910348497', 'iBank-CLIENT-3609', 'manalikundu824@gmail.com', 'AE-7/2 Jyangra Chowmatha Raghunathpur Kolkata - 700059', '2023-10-24 07:24:25.624314'),
+(21, 'Manali Kundu', '631025897', 'Savings ', '20', 'Active', '0', '11', 'Manali Kundu', 'staff@mail.com', '08910348497', 'iBank-CLIENT-3609', 'manalikundu824@gmail.com', 'AE-7/2 Jyangra Chowmatha Raghunathpur Kolkata - 700059', '2023-10-24 07:28:24.513118'),
+(22, 'Manali Kundu', '019642375', 'Savings ', '20', 'Active', '0', '12', 'Manali Kundu', 'manalikundu824@gmail.com', '08910348497', 'iBank-CLIENT-3124', 'manalikundu824@gmail.com', 'AE-7/2 Jyangra Chowmatha Raghunathpur Kolkata - 700059', '2023-10-24 07:31:18.669794'),
+(23, 'Manali Kundu', '289704165', ' Retirement ', '10', 'Active', '0', '12', 'Manali Kundu', 'manalikundu824@gmail.com', '08910348497', 'iBank-CLIENT-3124', 'manalikundu824@gmail.com', 'AE-7/2 Jyangra Chowmatha Raghunathpur Kolkata - 700059', '2023-11-15 19:38:02.521966'),
+(24, 'man', '273481609', 'Credit Card Accounts ', '10.75', 'Active', '0', '10', 'man', '2535', '325525626', 'iBank-CLIENT-2910', 'sam@mail.com', 'Enugu Nigeria', '2023-11-15 20:03:43.159682');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_clients`
+--
+
+CREATE TABLE `ib_clients` (
+  `client_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `national_id` varchar(200) NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `profile_pic` varchar(200) NOT NULL,
+  `client_number` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ib_clients`
+--
+
+INSERT INTO `ib_clients` (`client_id`, `name`, `national_id`, `phone`, `address`, `email`, `password`, `profile_pic`, `client_number`) VALUES
+(10, 'man', '2535', '325525626', 'Enugu Nigeria', 'sam@mail.com', '903b21879b4a60fc9103c3334e4f6f62cf6c3a2d', '', 'iBank-CLIENT-2910'),
+(12, 'Manali Kundu', 'manalikundu824@gmail.com', '08910348497', 'AE-7/2 Jyangra Chowmatha Raghunathpur Kolkata - 700059', 'manalikundu824@gmail.com', '2fabaf84a02f6b7b62c87436fe6158f806dc7d7f', '', 'iBank-CLIENT-3124');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_notifications`
+--
+
+CREATE TABLE `ib_notifications` (
+  `notification_id` int(20) NOT NULL,
+  `notification_details` text NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_staff`
+--
+
+CREATE TABLE `ib_staff` (
+  `staff_id` int(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `staff_number` varchar(200) NOT NULL,
+  `phone` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `sex` varchar(200) NOT NULL,
+  `profile_pic` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ib_staff`
+--
+
+INSERT INTO `ib_staff` (`staff_id`, `name`, `staff_number`, `phone`, `email`, `password`, `sex`, `profile_pic`) VALUES
+(3, 'Staff ', 'iBank-STAFF-6785', '0704975742', 'staff@mail.com', 'e97bb656674116ade9d0baab9a54fefeca643dea', 'Male', 'user-profile-min.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_systemsettings`
+--
+
+CREATE TABLE `ib_systemsettings` (
+  `id` int(20) NOT NULL,
+  `sys_name` longtext NOT NULL,
+  `sys_tagline` longtext NOT NULL,
+  `sys_logo` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `ib_systemsettings`
+--
+
+INSERT INTO `ib_systemsettings` (`id`, `sys_name`, `sys_tagline`, `sys_logo`) VALUES
+(1, 'Credit Card Management System', 'Financial success at every service we offer.', 'ibankinglg.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ib_transactions`
+--
+
+CREATE TABLE `ib_transactions` (
+  `tr_id` int(20) NOT NULL,
+  `tr_code` varchar(200) NOT NULL,
+  `account_id` varchar(200) NOT NULL,
+  `acc_name` varchar(200) NOT NULL,
+  `account_number` varchar(200) NOT NULL,
+  `acc_type` varchar(200) NOT NULL,
+  `acc_amount` varchar(200) NOT NULL,
+  `tr_type` varchar(200) NOT NULL,
+  `tr_status` varchar(200) NOT NULL,
+  `client_id` varchar(200) NOT NULL,
+  `client_name` varchar(200) NOT NULL,
+  `client_national_id` varchar(200) NOT NULL,
+  `transaction_amt` varchar(200) NOT NULL,
+  `client_phone` varchar(200) NOT NULL,
+  `receiving_acc_no` varchar(200) NOT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `receiving_acc_name` varchar(200) NOT NULL,
+  `receiving_acc_holder` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ib_acc_types`
+--
+ALTER TABLE `ib_acc_types`
+  ADD PRIMARY KEY (`acctype_id`);
+
+--
+-- Indexes for table `ib_admin`
+--
+ALTER TABLE `ib_admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `ib_bankaccounts`
+--
+ALTER TABLE `ib_bankaccounts`
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- Indexes for table `ib_clients`
+--
+ALTER TABLE `ib_clients`
+  ADD PRIMARY KEY (`client_id`);
+
+--
+-- Indexes for table `ib_notifications`
+--
+ALTER TABLE `ib_notifications`
+  ADD PRIMARY KEY (`notification_id`);
+
+--
+-- Indexes for table `ib_staff`
+--
+ALTER TABLE `ib_staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- Indexes for table `ib_systemsettings`
+--
+ALTER TABLE `ib_systemsettings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ib_transactions`
+--
+ALTER TABLE `ib_transactions`
+  ADD PRIMARY KEY (`tr_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ib_acc_types`
+--
+ALTER TABLE `ib_acc_types`
+  MODIFY `acctype_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `ib_admin`
+--
+ALTER TABLE `ib_admin`
+  MODIFY `admin_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ib_bankaccounts`
+--
+ALTER TABLE `ib_bankaccounts`
+  MODIFY `account_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `ib_clients`
+--
+ALTER TABLE `ib_clients`
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `ib_notifications`
+--
+ALTER TABLE `ib_notifications`
+  MODIFY `notification_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `ib_staff`
+--
+ALTER TABLE `ib_staff`
+  MODIFY `staff_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ib_systemsettings`
+--
+ALTER TABLE `ib_systemsettings`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ib_transactions`
+--
+ALTER TABLE `ib_transactions`
+  MODIFY `tr_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ staff/conf/config.php
@@ -0,0 +1,6 @@
+<?php
+    $dbuser="root";
+    $dbpass="";
+    $host="localhost";
+    $db="creditcardmanagementsystem_db";
+    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
